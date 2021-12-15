7af8eb4bd9cb4fe0556f5f5e2985e43eb2f12d58
youssif hany <youssifhany75@gmail.com>
2021-12-15 21:01:02 +0200

    fix image-width

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import classes from "./CTA.module.css";
 
 function CTA() {
-  return <div></div>;
+  return (
+    <div className={classes["gpt3__cta"]}>
+      <div className={classes["gpt3__cta-content"]}>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className={classes["gpt3__cta-btn"]}>
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 }
 
 export default CTA;
